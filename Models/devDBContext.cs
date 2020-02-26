7b94a4e0e39fe727259c956ef47979acd678fee4
Saidur Rahman <saidurwd@gmail.com>
2020-02-26 19:34:07 +0600

    Project initialize

---
+++ Models/devDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OptimoCore.Models;
+
+namespace OptimoCore.Data
+{
+    public class devDBContext : DbContext
+    {
+        public devDBContext(DbContextOptions<devDBContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<OptimoCore.Models.Product> Product { get; set; }
+    }
+}
